815dd83e359621c808c61d3f78409c6100fff447
Bryan`Y. Limz <superuser@brylimz.com>
2021-12-01 14:02:20 +0700

    turtle object

--- day16.py
+++ day16.py
@@ -1,11 +1,14 @@
+import turtle
 from turtle import Turtle, Screen
 timmy = Turtle()
+turtle.title("Well Played")
 timmy.shape("turtle")
 timmy.color("white")
+timmy.forward(100)
 print(timmy)
 
 my_screen = Screen()
-print(my_screen.canvheight)
 my_screen.bgcolor("green")
 my_screen.exitonclick()
+print(my_screen.canvheight)
 
