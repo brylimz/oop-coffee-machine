cea727eed4bfab71b7c73ab128c891ba4827f0dd
Bryan`Y. Limz <superuser@brylimz.com>
2021-12-01 13:12:54 +0700

    day 16 starts

--- day16.py
+++ day16.py
@@ -1,6 +1,11 @@
 from turtle import Turtle, Screen
 timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("white")
 print(timmy)
 
 my_screen = Screen()
 print(my_screen.canvheight)
+my_screen.bgcolor("green")
+my_screen.exitonclick()
+
