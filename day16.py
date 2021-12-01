988d2933f3e211b697d274f1672a2f9b6cbeb4a3
Bryan`Y. Limz <superuser@brylimz.com>
2021-12-01 15:42:31 +0700

    package exercise

--- day16.py
+++ day16.py
@@ -13,7 +13,12 @@
 # print(my_screen.canvheight)
 
 from prettytable import PrettyTable
+
 table = PrettyTable()
+
 table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
-table.add_column("Type", ["Electric", "Water","Fire"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "l"
 print(table)
+
+
