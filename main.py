4f4f0f720770d4f6c643569017495ba03b3d7226
Bryan`Y. Limz <superuser@brylimz.com>
2021-12-01 18:35:09 +0700

    while loop added

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+is_on = True
+
+coffee_maker.report()
+money_machine.report()
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? {options}")
