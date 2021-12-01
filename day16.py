24d56cfc0d64cf2133eef09c510056080ce4737b
Bryan`Y. Limz <superuser@brylimz.com>
2021-12-01 14:34:19 +0700

    package exercise

--- day16.py
+++ day16.py
@@ -1,14 +1,19 @@
-import turtle
-from turtle import Turtle, Screen
-timmy = Turtle()
-turtle.title("Well Played")
-timmy.shape("turtle")
-timmy.color("white")
-timmy.forward(100)
-print(timmy)
+# import turtle
+# from turtle import Turtle, Screen
+# timmy = Turtle()
+# turtle.title("Well Played")
+# timmy.shape("turtle")
+# timmy.color("white")
+# timmy.forward(100)
+# print(timmy)
+#
+# my_screen = Screen()
+# my_screen.bgcolor("green")
+# my_screen.exitonclick()
+# print(my_screen.canvheight)
 
-my_screen = Screen()
-my_screen.bgcolor("green")
-my_screen.exitonclick()
-print(my_screen.canvheight)
-
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water","Fire"])
+print(table)
